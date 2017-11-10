43ad2df48d097ae78694957c7990fc79ea7f6dd1
Ayswarya Padmanabhan <ayswarya.padmanabhan@oulu.fi>
2017-11-10 10:34:54 +0200

    startup script

---
+++ startup.m
@@ -0,0 +1,25 @@
+
+fprintf('Adding cvx path !! \n');
+cDirectory = pwd;
+
+cd cvx-w64;
+cvx_setup('cvx_license.dat');
+cd(cDirectory);
+
+cvx_solver('Mosek');
+cvx_expert('true');
+cvx_quiet('true');
+
+lsDir = dir(cDirectory);
+[stringIndex, okButton] = listdlg('listString','Enter Workspace Directory :',...
+    'SelectionMode','Single','ListString',{lsDir.name},'ListSize',[200, 250]);
+
+if okButton
+    wDirectory = sprintf('%s%s%s',cDirectory,filesep,lsDir(stringIndex).name);
+    cd(wDirectory);
+    addpath(genpath(pwd));
+else
+    fprintf('No working path added !!');
+end
+
+clc;
